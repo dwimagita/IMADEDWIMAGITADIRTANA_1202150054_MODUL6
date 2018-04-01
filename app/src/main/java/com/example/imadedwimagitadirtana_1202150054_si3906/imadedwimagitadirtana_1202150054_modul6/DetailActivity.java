21f7058e348c154e33539a5ddc7252767a0471f5
dwima <dwima97@gmail.com>
2018-04-01 14:10:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/imadedwimagitadirtana_1202150054_modul6/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.imadedwimagitadirtana_1202150054_si3906.imadedwimagitadirtana_1202150054_modul6;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/example/imadedwimagitadirtana_1202150054_si3906/imadedwimagitadirtana_1202150054_modul6/Post.java
@@ -0,0 +1,41 @@
+package com.example.imadedwimagitadirtana_1202150054_si3906.imadedwimagitadirtana_1202150054_modul6;
+
+
+public class Post {
+    private String username;
+    private String photo;
+    private String photoTitle;
+    private String photoDesc;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhotoTitle() {
+        return photoTitle;
+    }
+
+    public void setPhotoTitle(String photoTitle) {
+        this.photoTitle = photoTitle;
+    }
+
+    public String getPhotoDesc() {
+        return photoDesc;
+    }
+
+    public void setPhotoDesc(String photoDesc) {
+        this.photoDesc = photoDesc;
+    }
+}
